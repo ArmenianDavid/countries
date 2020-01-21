c2cb7aca041c5dce83f303d01f0ccc8f17859f63
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-01-21 16:58:53 +0400

    Updates

---
+++ precache-manifest.d0758adf98b479d5255d5d7d36499131.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d58a5141d03d7c89dea7ccf78ad4353e",
+    "url": "/counties/index.html"
+  },
+  {
+    "revision": "ea42fee5aa0ee331691a",
+    "url": "/counties/static/js/2.03e8a39f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/counties/static/js/2.03e8a39f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "60ff8ea147bc766fc54e",
+    "url": "/counties/static/js/main.641b41e1.chunk.js"
+  },
+  {
+    "revision": "f1279bda06a79d3f66f9",
+    "url": "/counties/static/js/runtime-main.81a4b951.js"
+  }
+]);
