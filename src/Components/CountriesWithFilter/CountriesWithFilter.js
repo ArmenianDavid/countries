46252ac4ae5f844013b158d23f7798fbddc1bb08
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-01-25 15:25:00 +0400

    style : added fetch and loading

--- src/Components/CountriesWithFilter/CountriesWithFilter.js
+++ src/Components/CountriesWithFilter/CountriesWithFilter.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import LinearProgress from '@material-ui/core/LinearProgress';
 
 class CountriesWithFilter extends React.Component{
   constructor(){
@@ -9,7 +10,33 @@ class CountriesWithFilter extends React.Component{
     }
   }
 
+  componentDidMount(){
+    this.setState({
+      loading : true,
+    })
+    setTimeout(() =>
+      fetch('https://restcountries.eu/rest/v2/all')
+      .then(res => res.json())
+      .then(json => json)
+      .then(data =>  this.setState({ data : data , loading : false}))
+      .catch(error => {
+        this.setState({
+          error : error.errorMessage,
+          loading : false,
+        })
+      }),1000)
+      
+  }
+
   render(){
+    const { data , loading } = this.state
+    console.log(data)
+    if(loading){
+      return( <div>
+                <LinearProgress variant="query" color="primary" />
+              </div>
+              )
+    }
     return(
       <div>
         Hello from CountriesWithFilter
