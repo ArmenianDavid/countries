bc6c98412fdb398ab0568c9f92542e1d0e9a5dd9
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-01-26 23:27:30 +0400

    feat : rendered to the DOM by functional component using hooks

--- src/Components/WithHooks/WithHooks.js
+++ src/Components/WithHooks/WithHooks.js
@@ -1,13 +1,45 @@
-import React , {useState} from 'react'
+import React , {useState , useEffect } from 'react'
+// components
+import './WithHooks.css';
 
 function WithHooks(){
-  const [ count , setCount ] = useState(0)
+
+  const [ data , setData ] = useState([])
+
+  useEffect(
+    () =>{
+      fetch('https://restcountries.eu/rest/v2/all')
+        .then(resp => resp.json())
+        .then(data => setData( data ))
+        .catch( error => console.log(error))
+    }
+  )
+
+  
+
   return(
-    <div>
-      <span>{count}</span>
-      <button onClick={() => setCount( count + 1 )}>
-        incriment
-      </button>
+    <div className='with-hooks-container'>
+      <table>
+        <thead>
+          <th>Country</th>
+          <th>Capital</th>
+          <th>Population</th>
+          <th>Flag</th>
+        </thead>
+
+        <tbody>
+          {data.map( country =>{
+            return(
+              <tr key={country.name}>
+                <td>{country.name}</td>
+                <td>{country.capital}</td>
+                <td>{country.population}</td>
+                <td> <img src={country.flag} alt='flag' width='40px' /> </td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
     </div>
   )
 }
