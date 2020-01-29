1acd7cbbf192151f8d3429b87157ecefa87567f2
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-01-29 12:39:10 +0400

    fix : bugs with hooks are fixed

--- src/Components/WithHooks/WithHooks.js
+++ src/Components/WithHooks/WithHooks.js
@@ -1,57 +1,67 @@
-import React , {useState , useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 // components
-import './WithHooks.css';
+import "./WithHooks.css";
 
-function WithHooks(){
+function WithHooks() {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
 
-  const [ data , setData ] = useState([])
-  const [ loading , setLoading ] = useState(false)
+    useEffect(
+        () => setLoading(true),
+        fetch("https://restcountries.eu/rest/v2/all")
+            .then(resp => resp.json())
+            .then(data => {
+                setLoading(false);
+                setData(data);
+                return data;
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    );
 
-  useEffect(
-     () => setLoading(true),
-      fetch('https://restcountries.eu/rest/v2/all')
-        .then(resp => resp.json())
-        .then(data => {
-          setData( data )
-          return data
-        })
-        .catch( error => console.log(error)),
-     () => setLoading(false)
-  )
+    if (loading) {
+        return (
+            <div className="loading-container">
+                <div>Loading....</div>
+            </div>
+        );
+    }
 
-  // if(loading){
-  //   return (
-  //     <div>
-  //       Loading....
-  //     </div>
-  //   )
-  // }
+    return (
+        <div className="with-hooks-container">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Country</th>
+                        <th>Capital</th>
+                        <th>Population</th>
+                        <th>Flag</th>
+                    </tr>
+                </thead>
 
-  return(
-    <div className='with-hooks-container'>
-      <table>
-        <thead>
-          <th>Country</th>
-          <th>Capital</th>
-          <th>Population</th>
-          <th>Flag</th>
-        </thead>
-
-        <tbody>
-          {data.map( country =>{
-            return(
-              <tr key={country.name}>
-                <td>{country.name}</td>
-                <td>{country.capital}</td>
-                <td>{country.population}</td>
-                <td> <img src={country.flag} alt='flag' width='40px' /> </td>
-              </tr>
-            )
-          })}
-        </tbody>
-      </table>
-    </div>
-  )
+                <tbody>
+                    {data.map(country => {
+                        return (
+                            <tr key={country.name}>
+                                <td>{country.name}</td>
+                                <td>{country.capital}</td>
+                                <td>{country.population}</td>
+                                <td>
+                                    {" "}
+                                    <img
+                                        src={country.flag}
+                                        alt="flag"
+                                        width="40px"
+                                    />{" "}
+                                </td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
-export default WithHooks
+export default WithHooks;
