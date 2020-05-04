041969b85f5bd79755d829ac51156e66c48391d8
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-05-04 23:24:13 +0400

    Remove ButtonGroup from Navigation

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -4,37 +4,34 @@ import { Link } from "react-router-dom";
 import "./Navigation.css";
 // Material ui
 import Button from "@material-ui/core/Button";
-import ButtonGroup from "@material-ui/core/ButtonGroup";
 
-const Navigation = () => {
+const Navigation = (props) => {
     return (
         <div className="navigation-container">
             <div className="navigation">
-                <ButtonGroup>
-                    <Link style={{ textDecoration: "none" }} to="/">
-                        <Button color="primary" aria-label="outlined primary ">
-                            Countries
-                        </Button>
-                    </Link>
-                    <Link style={{ textDecoration: "none" }} to="/material-ui">
-                        <Button color="primary" aria-label="outlined primary ">
-                            MaterialUi
-                        </Button>
-                    </Link>
-                    <Link
-                        style={{ textDecoration: "none" }}
-                        to="/countries-with-filter"
-                    >
-                        <Button color="primary" aria-label="outlined primary ">
-                            Countries With filter
-                        </Button>
-                    </Link>
-                    <Link style={{ textDecoration: "none" }} to="/with-hooks">
-                        <Button color="primary" aria-label="outlined primary ">
-                            With Hooks
-                        </Button>
-                    </Link>
-                </ButtonGroup>
+                <Link style={{ textDecoration: "none" }} to="/">
+                    <Button color="primary" aria-label="outlined primary ">
+                        Countries
+                    </Button>
+                </Link>
+                <Link style={{ textDecoration: "none" }} to="/material-ui">
+                    <Button color="primary" aria-label="outlined primary ">
+                        MaterialUi
+                    </Button>
+                </Link>
+                <Link
+                    style={{ textDecoration: "none" }}
+                    to="/countries-with-filter"
+                >
+                    <Button color="primary" aria-label="outlined primary ">
+                        Countries With filter
+                    </Button>
+                </Link>
+                <Link style={{ textDecoration: "none" }} to="/with-hooks">
+                    <Button color="primary" aria-label="outlined primary ">
+                        With Hooks
+                    </Button>
+                </Link>
             </div>
         </div>
     );
